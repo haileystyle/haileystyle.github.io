3fc520a2b0e71e1a950eee2a2bd5517ec3c43793
sanmu <hoso1312@gmail.com>
2019-01-17 13:09:22 +1100

    init

---
+++ precache-manifest.a06e49d0e9ab076420f57c29b62955ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bbf32f751736d37c1b7e",
+    "url": "/static/js/main.bbf32f75.chunk.js"
+  },
+  {
+    "revision": "846e098a801d8cb491f7",
+    "url": "/static/js/1.846e098a.chunk.js"
+  },
+  {
+    "revision": "bbf32f751736d37c1b7e",
+    "url": "/static/css/main.094703d7.chunk.css"
+  },
+  {
+    "revision": "acdbf1f20f969820f1c37cefee71cb20",
+    "url": "/index.html"
+  }
+];
